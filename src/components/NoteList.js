9911a452d4cbcc9e35f60d26362e1b066d5ba8c5
cubanFree <alvarofuentesbarrera@gmail.com>
2024-06-06 22:44:28 -0700

    funcion de eliminado de posts

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 const NoteList = ({ notes }) => {
+
+    const deleteNote = async (event, id) => {
+        event.preventDefault();
+        try {
+            await fetch(`http://localhost:5000/api/notes/${id}`, {
+                method: 'DELETE',
+            });
+            window.location.reload();
+        } catch (error) {
+            console.error("Error deleting note:", error);
+        }
+    }
+
     if (!Array.isArray(notes)) {
         return <div>No notes available</div>;
     }
 
     return (
-        <div>
+        <div className="note-list-container">
             {notes.map((note) => (
                 <div className="note" key={note.id}>
-                    <h2>{note.title}</h2>
-                    <p>{note.content}</p>
+                    <article>
+                        <h2>{note.title}</h2>
+                        <p>{note.content}</p>
+                    </article>
+                    <span className="span-delete" onClick={(event) => deleteNote(event, note.id)}>Eliminar</span>
                 </div>
             ))}
         </div>
